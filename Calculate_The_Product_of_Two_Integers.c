d04e0660272686a20d33e4e32bd4a32dd246634c
EmmanuelDomenech <82624469+EmmanuelDomenech@users.noreply.github.com>
2021-04-19 17:25:04 -0400

    Update Calculate_The_Product_of_Two_Integers.c

--- Calculate_The_Product_of_Two_Integers.c
+++ Calculate_The_Product_of_Two_Integers.c
@@ -10,6 +10,13 @@ int main()
   x=-6;
   y=4;
   z= x * y;
-  printf("Integers (x,y) and sum (z) are : %d,%d,%d \n", x,y,z);
+  printf("Integers (x,y) and (z) are : %d,%d,%d \n", x,y,z);
   return 0;
 }
+/* Notes:
+Line number 10 “x=-6;” is a negative integer number.
+Line number 11 “y=4;” is a positive integer number.
+Line number 12 “z= x * y;” will return your integer number with the appropriate symbol if the result is negative, in this case, -24.
+
+Want to input your values then calculate the product of the two integers? Change the integers on lines 10 & 11. Have fun!
+/*
