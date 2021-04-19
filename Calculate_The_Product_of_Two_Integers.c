577b1111271fc4899fe1c5a56f70ae2ffe167c10
EmmanuelDomenech <82624469+EmmanuelDomenech@users.noreply.github.com>
2021-04-19 17:10:33 -0400

    Create Calculate_The_Product_of_Two_Integers.c

---
+++ Calculate_The_Product_of_Two_Integers.c
@@ -0,0 +1,15 @@
+// C code
+//This program will calculate the product of two integer number to yield a third integer number.
+
+#include <stdio.h>
+int main() 
+{
+  /* Variable definition: */
+  int x, y, z;
+  /* Variable initialization */
+  x=-6;
+  y=4;
+  z= x * y;
+  printf("Integers (x,y) and sum (z) are : %d,%d,%d \n", x,y,z);
+  return 0;
+}
